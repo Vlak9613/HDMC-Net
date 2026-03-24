065e6fb6926ef9be32ca555346b889bc08a0e131
Vlak9613 <zhangshuai@tongji.edu.cn>
2026-03-24 16:42:30 +0800

    test

--- utils.py
+++ utils.py
@@ -9,12 +9,13 @@ import numpy as np
 
 from tqdm import tqdm
 
-# def import_class(name):
-    # components = name.split('.')
-    # mod = __import__(components[0])
-    # for comp in components[1:]:
-        # mod = getattr(mod, comp)
-    # return mod
+
+def import_class(name):
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
 
 def import_class(import_str):
     mod_str, _sep, class_str = import_str.rpartition('.')
